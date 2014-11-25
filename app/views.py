e0741218786bcba5357f4240e74ba3aef4d3318b
EricSchles <ericschles@gmail.com>
2014-11-24 16:01:50 -0800

    finished view functions

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for, jsonify
+from flask import render_template, request, redirect, url_for, jsonify, flash
 
 @app.route("/", methods=["GET","POST"])
 @app.route("/index", methods=["GET","POST"])
@@ -14,3 +14,20 @@ def add(first='',second=''):
         second = int(request.get("second"))
     result = first + second
     return render_template("index.html",result=result)
+
+@app.route("/check_length")
+def check_length():
+    username = request.get("username")
+    if len(username) > 15:
+        flash("You entered a very long user name..Please reconsider your life choices")
+    
+@app.route("send_data")
+def send_data():
+    data={"Eric Schles":"eric.schles@syncano.com",
+          "job":"developer evangelist",
+          "mission":"end slavery",
+          "training for":"the olympics",
+          "hobbies":["guitar","rock climbing"],
+          "friends":"everyone"
+          }
+    return jsonify(data)
